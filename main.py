f83d9a3efdb1960bd325872ca56b276bc2e56a39
DogiJ <dogidogi159753@gmail.com>
2014-06-05 20:39:40 +0800

    modify main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ class configuration():
     def __init__(self, camid):
         self.key_fire_interval = 0.5  # seconds
         self.camid = camid
-        self.cam = None
         self.thresh = 127
         self.dilate_iterations = 2
         self.corner_position = np.zeros((4, 2))
+
     def config_CAM_parameters(self):
         check = 'n'
         while check == 'n':
@@ -22,43 +22,44 @@ class configuration():
             dilate_iterations = input('Please input dilate interations:')
             print 'Please touch the laser keyboard.(press q to close the camera)'
             time.sleep(1)
-            self.cam = pylaserkbd.CAM(self.camid, thresh, dilate_iterations)
+            cam = pylaserkbd.CAM(self.camid, thresh, dilate_iterations)
             for i in range(100):
-                self.cam.query()
-                charpts, contours = self.cam.retrieve()
-                self.cam.show()
+                cam.query()
+                charpts, contours = cam.retrieve()
+                cam.show()
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
-            self.cam.release()
-            cv2.destroyAllWindows()
+            cam.close()
             check = raw_input('Are the parameters okay?(y/n):')
         self.thresh = thresh
         self.dilate_iterations = dilate_iterations
+
     def mapping_calibration(self):
         corner = ['左上角', '右上角', '左下角', '右下角']
         for n in range(4):
             print 'Please put your finger at', corner[n]
             os.system("pause")  # press any key to continue
-            # time.sleep(2)
             img = pylaserkbd.CAM(self.camid, self.thresh, self.dilate_iterations)
             img.query()
             charpts, contours = img.retrieve()
             print 'Ok, you can leave yuor finger.'
-            # self.corner_position[n] = charpts
+            self.corner_position[n] = charpts
             img.release()
-            # time.sleep(2)
         print 'Calibration done.'
+
     def save(self):
         # save configuration paraeters to file
         pass
+
     def load(self):
         try:
             '''try to load configuration'''
         except:
             pass
             # raise assertion error 
+
 if __name__ == '__main__':
-    config = configuration()
+    config = configuration(1)
     try:
         config.load()
         '''try to load the configuration from file.'''
@@ -69,5 +70,4 @@ if __name__ == '__main__':
         config.save()
     cam = pylaserkbd.CAM(config.camid)
     while time.sleep(config.key_fire_interval):
-        """typing session"""
-        pass        
+        
